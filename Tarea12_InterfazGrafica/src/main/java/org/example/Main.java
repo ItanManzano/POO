391dabd85150d7cd4baf0b884a614c02294fe3c2
ItanManzano <113070570+ItanManzano@users.noreply.github.com>
2023-05-01 18:48:09 -0600

    Add files via upload

---
+++ Tarea12_InterfazGrafica/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Principal ventana = new Principal("Hola munde AWT");
+
+    }
+}
---
+++ Tarea12_InterfazGrafica/src/main/java/org/example/iu/eventos/EventosRaton.java
@@ -0,0 +1,32 @@
+package org.example.iu.eventos;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EventosRaton implements MouseListener {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
