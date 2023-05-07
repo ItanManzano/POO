66d679405554ef8967aeb37b931b3df76a272de6
ItanManzano <113070570+ItanManzano@users.noreply.github.com>
2023-05-07 11:31:23 -0600

    Add files via upload

---
+++ ActividadExtra3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        //Ventana ventana = new Ventana("Layouts");
+
+       // VentanaGrid ventana = new VentanaGrid("Ventana Layout Cuadricula");
+        VentanaSwing ventanaSwing= new VentanaSwing("Centígrados a farenheid");
+    }
+}
